dbe28117abc02615ff0adf0bf6f43ca7cd6519f6
markgietl <markgietl4@gmail.com>
2021-12-27 09:44:56 +0200

    first commit

---
+++ Shapes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
